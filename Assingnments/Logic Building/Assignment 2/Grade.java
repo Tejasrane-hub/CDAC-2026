ec2063b6a6cf1aacfcfe59f89654b1f6ea036dff
Tejas Rane <tejas.rane.cmaug26@gmail.com>
2026-08-18 23:00:57 +0530

    Add files via upload

---
+++ Assingnments/Logic Building/Assignment 2/Grade.java
@@ -0,0 +1,33 @@
+public class Grade
+{
+	public static void main(String args[])
+	{
+		int math = 80;
+		int science = 85;
+		int history = 90;
+		
+		int avg = (math+science+history)/3;
+		System.out.println("Average Marks : "+avg);
+		
+		if(avg>=90)
+		{
+			System.out.println("Grade A");
+		}
+		else if(avg>=70 && avg<=89)
+		{
+			System.out.println("Grade B");
+		}
+		else if(avg>=50 && avg<=69)
+		{
+			System.out.println("Grade C");
+		}
+		else if(avg>=30 && avg<=49)
+		{
+			System.out.println("Grade D");
+		}
+		else
+		{
+			System.out.println("Fail");
+		}
+	}
+}
---
+++ Assingnments/Logic Building/Assignment 2/Leap.java
@@ -0,0 +1,16 @@
+public class Leap
+{
+	public static void main(String args[])
+	{
+		int year = 2024;
+		
+		if(year%4==0 && year%100!=0 || year%400==0)
+		{
+			System.out.println(year +" Is a Leap Year");
+		}
+		else
+		{
+			System.out.println(year +" Is Not A Leap Year");
+		}	
+	}
+}
